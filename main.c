a923f96ce2974c54074f8c0dff6e4c95129bbf7f
NathanielBSmartt <nathaniel.bucknor-smartt@my.utsa.edu>
2025-01-28 15:24:51 +0000

    did it :)

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
+
+  int a;
+  scanf("%d", &a);
+
+  if (a % 3 == 0)
+  {
+    printf("CS");
+  }
+  else if (a % 5 == 0)
+  {
+    printf("1714");
+  }
+  else if (a % 5 == 0 && a % 3 == 0)
+  {
+    printf("CS1714");
+  }
+  else
+  {
+    printf("ERROR");
+  }
 
   return 0;
 }
